6b624a4c91948dbdd4f26c3ca59b4673f0db89d8
smedagli <s.medagli@gmail.com>
2022-09-02 19:10:35 +0200

    add argument parser for train_model

--- ml_prod/starter/train_model.py
+++ ml_prod/starter/train_model.py
@@ -8,19 +8,22 @@ from sklearn.model_selection import train_test_split
 from typing import List
 from pathlib import Path
 import pandas as pd
+import argparse
 
 from ml_prod.starter import common
 from ml_prod.starter.ml.model import Model
 from ml_prod.starter.ml.performance import PerformanceEvaluator
 from ml_prod.starter.ml import data
 
-# TODO: remove this when complete
-pd.options.display.width = 2500
-pd.options.display.max_columns = 25
 
-_DO_TRAINING = False
-_VERBOSE = True
-_ENCODER_TYPE = 'le'
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', action='store_true', default=False,
+                        help='Do testing (inference) instead of training')
+    parser.add_argument('-v', '--verbose', default=1, help='Set level of verbosity (0 to 2)')
+    parser.add_argument('-e', '--encoder_type', default='OHE', type=str,
+                        help='Encoding for categorical features: OHE (One Hot Encoding) or LE (Label Encoder)')
+    return parser.parse_args()
 
 
 def train(model: Model, full_data: pd.DataFrame, train_data: pd.DataFrame, categorical_features: List[str], label: str,
@@ -58,6 +61,22 @@ def test(model: Model, test_data: pd.DataFrame, categorical_features: List[str],
 
 
 if __name__ == '__main__':
+    args = parse_args()
+    _DO_TRAINING = not args.test
+
+    # verbosity
+    verbosity = int(args.verbose)
+    _VERBOSE, _VERBOSE_MODEL = False, False
+    if verbosity == 0:
+        pass
+    elif verbosity == 1:
+        _VERBOSE_MODEL = True
+    elif verbosity == 2:
+        _VERBOSE, _VERBOSE_MODEL = True, True
+
+    _ENCODER_TYPE = args.encoder_type
+
+    # main
     data_ = data.read_data()
 
     train_df, test_df = train_test_split(data_, test_size=0.20)  # split dataset
