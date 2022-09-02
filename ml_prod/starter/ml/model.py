cfd77aab835df2c4bf74675de9518e9cad5e5fa9
smedagli <s.medagli@gmail.com>
2022-09-02 15:09:39 +0200

    remove functions for training-inference (already implemented in the class)

--- ml_prod/starter/ml/model.py
+++ ml_prod/starter/ml/model.py
@@ -7,43 +7,6 @@ import pickle
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import fbeta_score, precision_score, recall_score
-
-
-# Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
-
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
-    """
-
-    pass
-
-def inference(model, X):
-    """ Run model inferences and return the predictions.
-
-    Inputs
-    ------
-    model : ???
-        Trained machine learning model.
-    X : np.array
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.array
-        Predictions from the model.
-    """
-    pass
 
 
 class Model:
