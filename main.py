7eec3968f80b7e160f7a0e9fe6a1ca7244e6ef90
smedagli <s.medagli@gmail.com>
2022-09-08 23:20:11 +0200

    fix for dvc

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import Union
+import os
 
 import sys
 sys.path.append('../')
 from starter.ml import data, model, encoders
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # Load model
 model_ = model.Model(verbose=False)
